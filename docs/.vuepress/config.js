ebdd3f77a00e208b656cb037fe924d0c61e749e8
Alex F. Bustos P <alexfbp@gmail.com>
2025-05-26 00:25:48 -0500

    cicd: deploy also on GitHub pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,17 @@
 const path = require("path");
+
+const {DEPLOY_TARGET} = process.env;
+let base = "/";
+let publicPath = "/";
+
+if (DEPLOY_TARGET === "cdn") {
+  base = "/";
+  publicPath = "https://cdn.gocn.vip/beego/";
+}
+
 module.exports = {
   title: "Beego",
-  base: "/beego/",
+  base: base,
   toc: {
     includeLevel: [1, 2, 3, 4],
   },
@@ -28,30 +38,17 @@ module.exports = {
     },
   },
   configureWebpack: () => {
-    const NODE_ENV = process.env.NODE_ENV;
-    //判断是否是生产环境
-    if (NODE_ENV === "production") {
-      return {
-        output: {
-          publicPath: "https://cdn.gocn.vip/beego/",
+    return {
+      output: {
+        publicPath: publicPath,
+      },
+      resolve: {
+        //配置路径别名
+        alias: {
+          public: path.resolve(__dirname, "./public"),
         },
-        resolve: {
-          //配置路径别名
-          alias: {
-            public: path.resolve(__dirname, "./public"),
-          },
-        },
-      };
-    } else {
-      return {
-        resolve: {
-          //配置路径别名
-          alias: {
-            public: path.resolve(__dirname, "./public"),
-          },
-        },
-      };
-    }
+      },
+    };
   },
   themeConfig: {
     sidebar: "auto",
